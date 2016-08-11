ac9db98c10de980c93e870d76ebc9e40de04f0c7
Hrishikesh Date <Hrishikesh.Date@gmail.com>
2016-08-11 09:44:19 +0530

    Wait for the system time to be synced via ntp daemon This is useful when there is a long power outage and the Pi clock get completely out of step with the actual time

--- homeautomation/cron_scripts/tweet_powerup.sh
+++ homeautomation/cron_scripts/tweet_powerup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+/usr/sbin/ntp-wait -n 5 -s 60 -v
 /usr/bin/docker run --rm --cap-add SYS_RAWIO --device /dev/mem -v /etc/localtime:/etc/localtime:ro -v /etc/timezone:/etc/timezone:ro -e PYTHONUNBUFFERED=1 -v /HDPiAutomation/job_tweetpowerup:/data hrishikesh/tweepy4iot /usr/bin/python /data/tweetpowerup.py
